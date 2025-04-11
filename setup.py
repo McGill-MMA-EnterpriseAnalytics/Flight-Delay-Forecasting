abe877ce5cced0533491363788b14e078c413e58
nandinisankarabukta-git <snandini20005@gmail.com>
2025-04-11 11:10:57 -0400

    Project setup: Initialized directory structure with Cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project aims to predict flight delays using machine learning techniques.',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
